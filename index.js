831fad613f6fa4e8e24e1a68af01df7a7bd35f65
Fred <huertaserick@yahoo.ie>
2024-07-03 18:07:18 +0100

    Adding BMI Calculator

--- index.js
+++ index.js
@@ -9,87 +9,27 @@ function closeForm() {
   formContainer.classList.add("hidden");
 }
 
-const reviewers = [
-  {
-    name: "Fiona Hughes",
-    occupation: "Physician",
-    img: "images/physician.png",
-    stars: [4.5],
-    text: "Focus on flexibility! Yoga, Pilates, and barre classes. Great for improving mobility and core strength.",
-  },
-  {
-    name: "Alex Woodley",
-    occupation: "Bartender",
-    img: "images/bartender.png",
-    stars: [4.5],
-    text: "Welcoming gym for all! Cardio machines, group fitness classes (Zumba, Yoga), friendly staff. Great for beginners or social workouts.",
-  },
-  {
-    name: "Ji-hoon Park",
-    occupation: "Chef",
-    img: "images/chef.png",
-    stars: [2.5],
-    text: "Affordable gym with decent equipment. Can get crowded during peak hours and intimidating too. Good option for basic workouts.",
-  },
-  {
-    name: "Emily Grace Johnson",
-    occupation: "Attorney",
-    img: "images/attorney.png",
-    stars: [5],
-    text: "Get bendy! Yoga, Pilates, barre classes. Improve mobility and core strength.",
-  },
-  {
-    name: "Dwight Aldridge",
-    occupation: "Steelworker",
-    img: "images/steelworker.png",
-    stars: [5],
-    text: "Absolutely love this gym! The trainers are so motivating, and the variety of classes keeps things interesting. Plus, the equipment is top-notch.",
-  },
-  {
-    name: "Miguel Cruz",
-    occupation: "Blogger",
-    img: "images/blogger.png",
-    stars: [5],
-    text: "This gym is a hidden gem! It's smaller than some of the chain gyms, but the personal attention from the trainers makes all the difference. Plus, the classes are challenging and fun.",
-  },
-  {
-    name: "Dr. Benjamin Stanley",
-    occupation: "Doctor",
-    img: "images/doctor.png",
-    stars: [5],
-    text: "The best gym in town! The facilities are always clean, and the trainers are incredibly knowledgeable. I've achieved amazing results thanks to their guidance.",
-  },
-  {
-    name: "Chioma Adekunle",
-    occupation: "Nurse",
-    img: "images/nurse.png",
-    stars: [5],
-    text: "Love the community vibe at this gym. Everyone is so supportive, and there's a real sense of camaraderie among members. Plus, the classes are fantastic!",
-  },
-  {
-    name: "Shanice Thompson",
-    occupation: "Call Agent",
-    img: "images/callAgent.png",
-    stars: [2],
-    text: "Not impressed with this gym. The equipment is often out of order, and it's always overcrowded during peak hours. Definitely not worth the membership fee.",
-  },
-  {
-    name: "Derek Kelly",
-    occupation: "Electrician",
-    img: "images/electrician.png",
-    stars: [5],
-    text: "Fun & energetic! Zumba, Hip-Hop, dance fitness classes. Burn calories and have a blast!",
-  },
-  {
-    name: "Josh Blessington",
-    occupation: "Engineer",
-    img: "images/engineer2.png",
-    stars: [4.5],
-    text: "Find your inner peace! Diverse yoga classes, experienced instructors, calming atmosphere. Perfect for relaxation and flexibility.",
-  },
-];
-
 // protein calculator
+
+let formProtein = document.querySelector(".form-protein");
+let proteinBtn = document.getElementById("proteinButton");
+let isOpen = false;
+
+function hideProtein() {
+  formProtein.classList.toggle("hidden");
+
+  if (isOpen) {
+    proteinBtn.innerHTML = "Protein Calculator";
+    proteinBtn.style.backgroundColor = "";
+    proteinBtn.style.color = "";
+  } else {
+    proteinBtn.innerHTML = "CLOSE";
+    proteinBtn.style.backgroundColor = "rgb(255, 76, 76)";
+    proteinBtn.style.color = "white";
+  }
+
+  isOpen = !isOpen;
+}
 
 function proteinCalculator() {
   let genderElem = document.getElementById("gender").value;
@@ -172,6 +112,86 @@ function proteinCalculator() {
 }
 
 // make the review randomized
+
+const reviewers = [
+  {
+    name: "Fiona Hughes",
+    occupation: "Physician",
+    img: "images/physician.png",
+    stars: [4.5],
+    text: "Focus on flexibility! Yoga, Pilates, and barre classes. Great for improving mobility and core strength.",
+  },
+  {
+    name: "Alex Woodley",
+    occupation: "Bartender",
+    img: "images/bartender.png",
+    stars: [4.5],
+    text: "Welcoming gym for all! Cardio machines, group fitness classes (Zumba, Yoga), friendly staff. Great for beginners or social workouts.",
+  },
+  {
+    name: "Ji-hoon Park",
+    occupation: "Chef",
+    img: "images/chef.png",
+    stars: [2.5],
+    text: "Affordable gym with decent equipment. Can get crowded during peak hours and intimidating too. Good option for basic workouts.",
+  },
+  {
+    name: "Emily Grace Johnson",
+    occupation: "Attorney",
+    img: "images/attorney.png",
+    stars: [5],
+    text: "Get bendy! Yoga, Pilates, barre classes. Improve mobility and core strength.",
+  },
+  {
+    name: "Dwight Aldridge",
+    occupation: "Steelworker",
+    img: "images/steelworker.png",
+    stars: [5],
+    text: "Absolutely love this gym! The trainers are so motivating, and the variety of classes keeps things interesting. Plus, the equipment is top-notch.",
+  },
+  {
+    name: "Miguel Cruz",
+    occupation: "Blogger",
+    img: "images/blogger.png",
+    stars: [5],
+    text: "This gym is a hidden gem! It's smaller than some of the chain gyms, but the personal attention from the trainers makes all the difference. Plus, the classes are challenging and fun.",
+  },
+  {
+    name: "Dr. Benjamin Stanley",
+    occupation: "Doctor",
+    img: "images/doctor.png",
+    stars: [5],
+    text: "The best gym in town! The facilities are always clean, and the trainers are incredibly knowledgeable. I've achieved amazing results thanks to their guidance.",
+  },
+  {
+    name: "Chioma Adekunle",
+    occupation: "Nurse",
+    img: "images/nurse.png",
+    stars: [5],
+    text: "Love the community vibe at this gym. Everyone is so supportive, and there's a real sense of camaraderie among members. Plus, the classes are fantastic!",
+  },
+  {
+    name: "Shanice Thompson",
+    occupation: "Call Agent",
+    img: "images/callAgent.png",
+    stars: [2],
+    text: "Not impressed with this gym. The equipment is often out of order, and it's always overcrowded during peak hours. Definitely not worth the membership fee.",
+  },
+  {
+    name: "Derek Kelly",
+    occupation: "Electrician",
+    img: "images/electrician.png",
+    stars: [5],
+    text: "Fun & energetic! Zumba, Hip-Hop, dance fitness classes. Burn calories and have a blast!",
+  },
+  {
+    name: "Josh Blessington",
+    occupation: "Engineer",
+    img: "images/engineer2.png",
+    stars: [4.5],
+    text: "Find your inner peace! Diverse yoga classes, experienced instructors, calming atmosphere. Perfect for relaxation and flexibility.",
+  },
+];
 
 function autoReview() {
   let reviewContent = document.querySelector(".review__content");
